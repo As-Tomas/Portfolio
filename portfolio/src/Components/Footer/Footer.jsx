e7eb16bf00bc9ea944c1a89c48519ddfdb5cd274
Tomas Bance <tomas.bance@gmail.com>
2023-08-03 11:55:09 +0200

    Footer simplified

--- portfolio/src/Components/Footer/Footer.jsx
+++ portfolio/src/Components/Footer/Footer.jsx
@@ -1,29 +1,26 @@
 const Footer = () => {
   return (
     <div className="group relative overflow-hidden">
-      <footer className=" z-10 fixed bottom-0 h-40 w-full mx-auto text-white text-xl bg-[rgba(255,255,255,0.2)] backdrop-blur-md border-t-2 rounded-t-2xl border-[rgba(255,255,255,0.3)] ">
-        <section className="max-w-4xl py-5 mx-auto p-4 flex flex-col sm:flex-row sm:justify-between">
+      <footer className=" z-10 fixed bottom-0 h-24  w-full mx-auto text-white text-xl bg-[rgba(255,255,255,0.2)] backdrop-blur-md border-t-2 rounded-t-2xl border-[rgba(255,255,255,0.3)] ">
+        <section className="max-w-4xl py-5 mx-auto p-4 flex  flex-row justify-between">
           <address>
-            <h2>You can find me.</h2>
-            5555 Bergen Norway
-            <br />
-            Email:{" "}
-            <a href="mailto:tomas.bance@gmail.com">tomas.bance@gmail.com</a>
-            <br />
-            Phone: <a href="tel:+4796889383">(+47) 968 893 83</a>
-          </address>
-          <nav className="hidden md:flex flex-col gap-2" aria-label="footer">
-            <a href="#home" className="hover:opacity-90">
-              Back to top
-            </a>
-            <a href="#technologies" className="hover:opacity-90">
-              Technologies
-            </a>
-            <a href="#contact" className="hover:opacity-90">
+          <a href="#contact" className="hover:opacity-90">
               Contact Me
             </a>
+            <br />
+            
+            <h2>
+            Email:
+            <a href="mailto:tomas.bance@gmail.com"> tomas.bance@gmail.com</a>
+            </h2>
+          </address>
+          <nav className="flex flex-col gap-2" aria-label="footer">
+            <h2>
+            Phone: <a href="tel:+4796889383">(+47) 968 893 83</a>
+            </h2>
+            
           </nav>
-          <div className="flex flex-col sm:gap-2">
+          <div className="hidden md:flex flex-col sm:gap-2">
             <p className="text-right">
               Copyright &copy; <span id="year">2023</span>
             </p>
@@ -32,8 +29,8 @@ const Footer = () => {
         </section>
       </footer>
       <div
-        className=" z-20 h-40 fixed bottom-0 w-full overflow-hidden before:bottom-0 before:absolute before:left-[-100%] rounded-t-2xl  
-      before:w-full before:h-40 before:content-'' before:bg-gradient-to-br before:transform before:rotate-45 before:from-transparent before:via-[rgba(255,255,255,0.3)]
+        className=" z-20 h-24 fixed bottom-0 w-full overflow-hidden before:bottom-0 before:absolute before:left-[-100%] rounded-t-2xl  
+      before:w-full before:h-24 before:content-'' before:bg-gradient-to-br before:transform before:rotate-45 before:from-transparent before:via-[rgba(255,255,255,0.3)]
       before:to-transparent before:transition-all before:duration-500 before:group-hover:left-[100%]"
       ></div>
     </div>
