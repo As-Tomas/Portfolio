d0486d2ffb2a47d7955955bd9c95c19da5c5d0d8
Tomas Bance <tomas.bance@gmail.com>
2023-08-04 23:47:04 +0200

    img paths updated

--- portfolio/src/data/ProjectsData.js
+++ portfolio/src/data/ProjectsData.js
@@ -2,22 +2,22 @@ const productData = [{
     "id": 0,
     "name": "Example Project",
     "description": "This is an example Project. This is an example Project. This is an example Project.",
-    "images": ["./src/data/tempPic.png"],
-    "technologies": ["./src/assets/csharp.svg", "./src/assets/wordpress.svg"]
+    "images": ["./data/tempPic.png"],
+    "technologies": ["./assets/csharp.svg", "./assets/wordpress.svg"]
   },
   {
     "id": 1,
     "name": "Example Project 2",
     "description": "This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2.",
-    "images": ["./src/data/tempPic.png", "./src/data/tempPic.png"],
-    "technologies": ["./src/assets/wordpress.svg", "./src/assets/dot_net.svg", "./src/assets/java.svg", "./src/assets/Microsoft_SQL_Server.png",]
+    "images": ["./data/tempPic.png", "./data/tempPic.png"],
+    "technologies": ["./assets/wordpress.svg", "./assets/dot_net.svg", "./assets/java.svg", "./assets/Microsoft_SQL_Server.png",]
   },
   {
     "id": 2,
     "name": "Example Project 2",
     "description": "This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2.",
-    "images": ["./src/data/tempPic.png", "./src/data/tempPic.png","./src/data/tempPic.png"],
-    "technologies": ["./src/assets/dot_net.svg", "./src/assets/csharp.svg", "./src/assets/wordpress.svg", "./src/assets/java.svg", "./src/assets/Microsoft_SQL_Server.png",]
+    "images": ["./data/tempPic.png", "./data/tempPic.png","./data/tempPic.png"],
+    "technologies": ["./assets/dot_net.svg", "./assets/csharp.svg", "./assets/wordpress.svg", "./assets/java.svg", "./assets/Microsoft_SQL_Server.png",]
   }
   
 ];
