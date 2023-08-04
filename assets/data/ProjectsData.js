953923286e9568207d49166642d6ee80f294c1ca
Tomas Bance <tomas.bance@gmail.com>
2023-08-04 23:54:23 +0200

    Updates

---
+++ assets/data/ProjectsData.js
@@ -0,0 +1,25 @@
+const productData = [{
+    "id": 0,
+    "name": "Example Project",
+    "description": "This is an example Project. This is an example Project. This is an example Project.",
+    "images": ["./data/tempPic.png"],
+    "technologies": ["./assets/csharp.svg", "./assets/wordpress.svg"]
+  },
+  {
+    "id": 1,
+    "name": "Example Project 2",
+    "description": "This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2.",
+    "images": ["./data/tempPic.png", "./data/tempPic.png"],
+    "technologies": ["./assets/wordpress.svg", "./assets/dot_net.svg", "./assets/java.svg", "./assets/Microsoft_SQL_Server.png",]
+  },
+  {
+    "id": 2,
+    "name": "Example Project 2",
+    "description": "This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2.",
+    "images": ["./data/tempPic.png", "./data/tempPic.png","./data/tempPic.png"],
+    "technologies": ["./assets/dot_net.svg", "./assets/csharp.svg", "./assets/wordpress.svg", "./assets/java.svg", "./assets/Microsoft_SQL_Server.png",]
+  }
+  
+];
+
+export default productData;
