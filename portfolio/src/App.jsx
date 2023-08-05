bea6649e96cd41b5bf554d97008dacf8e21f4063
Tomas Bance <tomas.bance@gmail.com>
2023-08-05 13:00:07 +0200

    Adding meniu for mobile

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -10,7 +10,13 @@ function App() {
     <>
       <div
         className="initial  "
-        // style={{ backgroundImage: `url(https://img.freepik.com/free-vector/abstract-blue-red-paper-cut-background-with-simple-shapes_8829-2520.jpg?w=1060&t=st=1690130003~exp=1690130603~hmac=be02e42313763296a7b2ca027bcf900b6d9d9fd851af8b294c959deb49b04d26)` }}
+         style={{ 
+          backgroundImage: `url(https://img.freepik.com/free-vector/abstract-blue-red-paper-cut-background-with-simple-shapes_8829-2520.jpg?w=1060&t=st=1690130003~exp=1690130603~hmac=be02e42313763296a7b2ca027bcf900b6d9d9fd851af8b294c959deb49b04d26)`,
+          backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center center",
+              boxSizing: "border-box",
+         }}
       >
         <Header setContent={setContent} />
         <div className="pt-20    ">
--- portfolio/src/Components/Header/Header.jsx
+++ portfolio/src/Components/Header/Header.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+
 const Header = ({ setContent }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
     
 
     <div className="group  ">
       <header className=" z-30 fixed top-0 left-0 w-full  h-16 px-20 py-5 bg-[rgba(255,255,255,0.1)]  mx-auto p-4 flex justify-between 
-      items-center backdrop-blur-md border-b-2 rounded-b-2xl   border-[rgba(255,255,255,0.2)] z-10">        
+      items-center  border-b-2 rounded-b-2xl   border-[rgba(255,255,255,0.2)] ">        
          
-         <h1 className="text-3xl font-medium">
+         <h1 className=" text-2xl sm:text-3xl font-medium">
            <button href="#hero"
             onClick={() => setContent('about')}
             > 
@@ -17,10 +21,12 @@ const Header = ({ setContent }) => {
            <button
              id="mobile-open-button"
              className="text-3xl sm:hidden focus:outline-none"
+             onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} 
            >
              &#9776;
            </button>
-           <nav className="hidden sm:block space-x-8 text-xl" aria-label="main">
+           <nav className={`sm:block text-xl ${isMobileMenuOpen ? 'z-50 flex flex-col justify-start items-start absolute p-6 pr-16 gap-5 top-full right-0 bg-[rgba(255,255,255,0.2)] backdrop-blur-md  border-l-2 border-b-2 border-r-2 border-[rgba(255,255,255,0.3)] shadow-md rounded-md' : 'hidden space-x-8'}`} aria-label="main">
+
              
              <a
                href="#about"
@@ -43,7 +49,7 @@ const Header = ({ setContent }) => {
        
         
       </header>
-      <div className=" z-20 fixed top-0 left-0 w-full overflow-x-hidden h-16 before:absolute before:top-0 before:left-[-100%] rounded-b-2xl
+      <div className=" z-20 fixed backdrop-blur-md top-0 left-0 w-full overflow-x-hidden h-16 before:absolute before:top-0 before:left-[-100%] rounded-b-2xl
    before:w-full before:h-full before:content-'' before:bg-gradient-to-r before:from-transparent before:via-[rgba(255,255,255,0.3)] 
    before:to-transparent before:transition-all before:duration-500 before:group-hover:left-[130%]"
       >
