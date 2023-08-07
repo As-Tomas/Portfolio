9d56bd552b7bde046d1f6173b6a3f01fbc4d1f27
Tomas Bance <tomas.bance@gmail.com>
2023-08-07 16:56:51 +0200

    projects date update

--- portfolio/public/data/ProjectsData.js
+++ portfolio/public/data/ProjectsData.js
@@ -1,6 +1,6 @@
 const productData = [{
     "id": 0,
-    "name": "Berlimo CMS/App",
+    "name": "Berlimo CMS + App",
     "description": "This project encompasses a cross-platform solution designed to streamline content management and delivery. The main project focuses is simplified management, user communication, and sending personalised information messages. On one end, we have a WordPress CMS seamlessly integrated with a .NET MAUI MVVM App, which acts as the client-side application. The app communicates directly with the WordPress API. This ensures that users of the .NET MAUI app always have access to the latest content updates and messages.",
     "images": ["./data/Projects_Imgs/berlimo/tempPic.png","./data/Projects_Imgs/berlimo/App.jpg","./data/Projects_Imgs/berlimo/projectStructure.png"],
     "technologies": ["./assets/dot_net.svg","./assets/wordpress.svg", "./assets/csharp.svg","./assets/figma.svg" ],
@@ -10,7 +10,7 @@ const productData = [{
     "id": 1,
     "name": "React Calculator",
     "description": "This is a recap of React and TailWind CSS themeing.",
-    "images": ["./data/Projects_Imgs/calc/calc2.png", "./data/Projects_Imgs/calc/calc1.png", "./data/Projects_Imgs/calc/calc3.png"],
+    "images": ["./data/Projects_Imgs/calc/calc3.png", "./data/Projects_Imgs/calc/calc1.png", "./data/Projects_Imgs/calc/calc2.png"],
     "technologies": ["./assets/react.svg", "./assets/tailwind.svg", "./assets/js.svg",],
     "links":["https://as-tomas.github.io/Calculator-App/"]
   },
@@ -18,9 +18,9 @@ const productData = [{
     "id": 2,
     "name": "Eletricity cost calculator",
     "description": "Simple Android App for calculation of electricity prices and relief based on consumed kilowatts.",
-    "images": ["./assets/staticSvg/photo.svg",],
+    "images": ["./data/Projects_Imgs/eletricityCalc/history.jpg","./data/Projects_Imgs/eletricityCalc/mainActivity.jpg"],
     "technologies": ["./assets/android.png", "./assets/java.svg",],
-    "links":[""]
+    "links":["https://github.com/As-Tomas/Eletricity_calculator"]
   },
   {
     "id": 3,
@@ -28,15 +28,15 @@ const productData = [{
     "description": "Back-End Java project for creating monthly data base and importing products from CSV file. ",
     "images": ["./data/backEnd.jpg",],
     "technologies": ["./assets/java.svg","./assets/Microsoft_SQL_Server.png",],
-    "links":[""]
+    "links":["https://github.com/As-Tomas/ProductsImporterSQLServerAndManagemet"]
   },
   {
     "id": 4,
     "name": "Product Scraper",
     "description": "Node.js automated product scraper by using Chrome/Chromium high-level API with the library Puppeteer and storing it in CSV file. ",
     "images": ["./data/backEnd.jpg",],
-    "technologies": ["./assets/node.svg","./data/Projects_Imgs/puppeteerScraper/puppeteer.png",],
-    "links":[""]
+    "technologies": ["./assets/node.svg","./data/Projects_Imgs/puppeteerScraper/puppeteer.png","./assets/js.svg"],
+    "links":["https://github.com/As-Tomas/Product_Scraper_VVS"]
   },
   {
     "id": 5,
@@ -50,7 +50,7 @@ const productData = [{
     "id": 6,
     "name": "FoofOut App",
     "description": "Android app, customers can register, make orders from selected restaurant and have it delivered straight to their homes.",
-    "images": ["./assets/staticSvg/photo.svg",],
+    "images": ["./data/Projects_Imgs/foodOut/delivery.jpg", "./data/Projects_Imgs/foodOut/login.jpg", "./data/Projects_Imgs/foodOut/menu.jpg"],
     "technologies": ["./assets/java.svg", "./assets/android.png",],
     "links":["https://github.com/As-Tomas/foodOut_Android_App"]
   },
@@ -58,7 +58,7 @@ const productData = [{
     "id": 7,
     "name": "Dreams and diary App",
     "description": "Android app, to make notes in diary format.",
-    "images": ["./assets/staticSvg/photo.svg",],
+    "images": ["./data/Projects_Imgs/dreamDiary/evaluation.jpg", "./data/Projects_Imgs/dreamDiary/form.jpg", "./data/Projects_Imgs/dreamDiary/records.jpg"],
     "technologies": ["./assets/java.svg", "./assets/android.png",],
     "links":["https://github.com/As-Tomas/Dreams_and_diary"]
   },
@@ -66,9 +66,17 @@ const productData = [{
     "id": 8,
     "name": "Speed Match game",
     "description": "Android Game: Different shapes slides from right to left, stops in the middle, and the player must choose buttons yes or no to answer. Does this symbol match the previous symbol? Score is calculated by the number of right answers in a given time.",
-    "images": ["./assets/staticSvg/photo.svg",],
+    "images": ["./data/Projects_Imgs/speedMatch/game.jpg", "./data/Projects_Imgs/speedMatch/rezults.jpg"],
     "technologies": ["./assets/java.svg", "./assets/android.png",],
     "links":["https://github.com/As-Tomas/Speed_Match_game"]
+  },
+  {
+    "id": 9,
+    "name": "PDF Invoice WP plugin ",
+    "description": "On customer request edited WooCommerce-PDF-Invoice template with extended table",
+    "images": ["./data/backEnd.jpg",],
+    "technologies": ["./assets/php.svg", "./assets/wordpress.svg",],
+    "links":["https://github.com/As-Tomas/WooCommerce-PDF-Invoice-template-extended-table"]
   }
   
 ];
