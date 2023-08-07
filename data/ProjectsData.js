8ed5cb62093ec9cf89a2e1ef072f36c19f9c008f
Tomas Bance <tomas.bance@gmail.com>
2023-08-07 14:26:13 +0200

    Updates

--- data/ProjectsData.js
+++ data/ProjectsData.js
@@ -1,23 +1,74 @@
 const productData = [{
     "id": 0,
-    "name": "Example Project",
-    "description": "This is an example Project. This is an example Project. This is an example Project.",
-    "images": ["./data/tempPic.png"],
-    "technologies": ["./assets/csharp.svg", "./assets/wordpress.svg"]
+    "name": "Berlimo CMS/App",
+    "description": "This project encompasses a cross-platform solution designed to streamline content management and delivery. The main project focuses is simplified management, user communication, and sending personalised information messages. On one end, we have a WordPress CMS seamlessly integrated with a .NET MAUI MVVM App, which acts as the client-side application. The app communicates directly with the WordPress API. This ensures that users of the .NET MAUI app always have access to the latest content updates and messages.",
+    "images": ["./data/Projects_Imgs/berlimo/tempPic.png","./data/Projects_Imgs/berlimo/App.jpg","./data/Projects_Imgs/berlimo/projectStructure.png"],
+    "technologies": ["./assets/dot_net.svg","./assets/wordpress.svg", "./assets/csharp.svg","./assets/figma.svg" ],
+    "links":["https://github.com/As-Tomas/BerlimoApp"]
   },
   {
     "id": 1,
-    "name": "Example Project 2",
-    "description": "This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2.",
-    "images": ["./data/tempPic.png", "./data/tempPic.png"],
-    "technologies": ["./assets/wordpress.svg", "./assets/dot_net.svg", "./assets/java.svg", "./assets/Microsoft_SQL_Server.png",]
+    "name": "React Calculator",
+    "description": "This is a recap of React and TailWind CSS themeing.",
+    "images": ["./data/Projects_Imgs/calc/calc2.png", "./data/Projects_Imgs/calc/calc1.png", "./data/Projects_Imgs/calc/calc3.png"],
+    "technologies": ["./assets/react.svg", "./assets/tailwind.svg", "./assets/js.svg",],
+    "links":["https://as-tomas.github.io/Calculator-App/"]
   },
   {
     "id": 2,
-    "name": "Example Project 2",
-    "description": "This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2. This is an example Project2.",
-    "images": ["./data/tempPic.png", "./data/tempPic.png","./data/tempPic.png"],
-    "technologies": ["./assets/dot_net.svg", "./assets/csharp.svg", "./assets/wordpress.svg", "./assets/java.svg", "./assets/Microsoft_SQL_Server.png",]
+    "name": "Eletricity cost calculator",
+    "description": "Simple Android App for calculation of electricity prices and relief based on consumed kilowatts.",
+    "images": ["./assets/staticSvg/photo.svg",],
+    "technologies": ["./assets/android.png", "./assets/java.svg",],
+    "links":[""]
+  },
+  {
+    "id": 3,
+    "name": "Product DataBase",
+    "description": "Back-End Java project for creating monthly data base and importing products from CSV file. ",
+    "images": ["./data/backEnd.jpg",],
+    "technologies": ["./assets/java.svg","./assets/Microsoft_SQL_Server.png",],
+    "links":[""]
+  },
+  {
+    "id": 4,
+    "name": "Product Scraper",
+    "description": "Node.js automated product scraper by using Chrome/Chromium high-level API with the library Puppeteer and storing it in CSV file. ",
+    "images": ["./data/backEnd.jpg",],
+    "technologies": ["./assets/node.svg","./data/Projects_Imgs/puppeteerScraper/puppeteer.png",],
+    "links":[""]
+  },
+  {
+    "id": 5,
+    "name": "Coding Game - spring chalenge 2021",
+    "description": "A Community competition Event to Learn and Code while Having Fun. ",
+    "images": ["./data/Projects_Imgs/springChalenge/victory.jpg",],
+    "technologies": ["./assets/Cplus.svg",],
+    "links":["https://github.com/As-Tomas/Spring_Chalenge_2021"]
+  },
+  {
+    "id": 6,
+    "name": "FoofOut App",
+    "description": "Android app, customers can register, make orders from selected restaurant and have it delivered straight to their homes.",
+    "images": ["./assets/staticSvg/photo.svg",],
+    "technologies": ["./assets/java.svg", "./assets/android.png",],
+    "links":["https://github.com/As-Tomas/foodOut_Android_App"]
+  },
+  {
+    "id": 7,
+    "name": "Dreams and diary App",
+    "description": "Android app, to make notes in diary format.",
+    "images": ["./assets/staticSvg/photo.svg",],
+    "technologies": ["./assets/java.svg", "./assets/android.png",],
+    "links":["https://github.com/As-Tomas/Dreams_and_diary"]
+  },
+  {
+    "id": 8,
+    "name": "Speed Match game",
+    "description": "Android Game: Different shapes slides from right to left, stops in the middle, and the player must choose buttons yes or no to answer. Does this symbol match the previous symbol? Score is calculated by the number of right answers in a given time.",
+    "images": ["./assets/staticSvg/photo.svg",],
+    "technologies": ["./assets/java.svg", "./assets/android.png",],
+    "links":["https://github.com/As-Tomas/Speed_Match_game"]
   }
   
 ];
