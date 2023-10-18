cc0cc8a1d4024682b47882da0ad39a7ac9194df8
Tomas Bance <tomas.bance@gmail.com>
2023-10-18 12:19:00 +0200

    adding project

--- portfolio/public/data/ProjectsData.js
+++ portfolio/public/data/ProjectsData.js
@@ -1,13 +1,21 @@
 const productData = [{
-    "id": 0,
-    "name": "Berlimo CMS + App",
-    "description": "This project encompasses a cross-platform solution designed to streamline content management and delivery. The main project focuses is simplified management, user communication, and sending personalised information messages. On one end, we have a WordPress CMS seamlessly integrated with a .NET MAUI MVVM App, which acts as the client-side application. The app communicates directly with the WordPress API. This ensures that users of the .NET MAUI app always have access to the latest content updates and messages.",
-    "images": ["./data/Projects_Imgs/berlimo/tempPic.png","./data/Projects_Imgs/berlimo/app.jpg","./data/Projects_Imgs/berlimo/projectStructure.png"],
-    "technologies": ["./assets/dot_net.svg","./assets/wordpress.svg", "./assets/csharp.svg","./assets/figma.svg" ],
-    "links":["https://github.com/As-Tomas/BerlimoApp"]
-  },
+  "id": 0,
+  "name": "Storyteller App",
+  "description": "Introducing an AI-powered storytelling app tailored for motivational and educational tales for children. At its core, this app leverages the capabilities of the Midjourney and ChatGPT APIs for dynamic content creation. Features: \n • A dedicated 'Parent Settings' section, allowing customization of story themes for motivational or educational purposes, along with other preferences tailored to your child's learning journey. \n • Voice input functionality, designed with a child-friendly interface for effortless interaction. Output options: \n • Text format, ideal for reading. \n • Text-to-speech feature, perfect for auditory learners.",
+  "images": ["./data/Projects_Imgs/Storyteller/welcomescrn.png","./data/Projects_Imgs/Storyteller/inputscrn.jpg","./data/Projects_Imgs/Storyteller/storyscrn.png"],
+  "technologies": ["./assets/react-native.png","./assets/ChatGPT-Logo.png", "./assets/Midjourney-logo.png","./assets/figma.svg" ],
+  "links":["https://github.com/As-Tomas/Storyteller-App"]
+},
+{
+  "id": 1,
+  "name": "Berlimo CMS + App",
+  "description": "This project encompasses a cross-platform solution designed to streamline content management and delivery. The main project focuses is simplified management, user communication, and sending personalised information messages. On one end, we have a WordPress CMS seamlessly integrated with a .NET MAUI MVVM App, which acts as the client-side application. The app communicates directly with the WordPress API. This ensures that users of the .NET MAUI app always have access to the latest content updates and messages.",
+  "images": ["./data/Projects_Imgs/berlimo/tempPic.png","./data/Projects_Imgs/berlimo/app.jpg","./data/Projects_Imgs/berlimo/projectStructure.png"],
+  "technologies": ["./assets/dot_net.svg","./assets/wordpress.svg", "./assets/csharp.svg","./assets/figma.svg" ],
+  "links":["https://github.com/As-Tomas/BerlimoApp"]
+},
   {
-    "id": 1,
+    "id": 2,
     "name": "Coding Game - spring chalenge 2021",
     "description": "A Community competition Event to Learn Code while Having Fun. I chose to code with C++ and reached the Gold League out of a total 8145 competitors. ",
     "images": ["./data/Projects_Imgs/springChalenge/start.png", "./data/Projects_Imgs/springChalenge/game.png", "./data/Projects_Imgs/springChalenge/victory.jpg",],
@@ -15,7 +23,7 @@ const productData = [{
     "links":["https://github.com/As-Tomas/Spring_Chalenge_2021"]
   },
   {
-    "id": 2,
+    "id": 3,
     "name": "React Calculator",
     "description": "This is a recap of React and TailWind CSS themeing.",
     "images": ["./data/Projects_Imgs/calc/calc3.png", "./data/Projects_Imgs/calc/calc1.png", "./data/Projects_Imgs/calc/calc2.png"],
@@ -23,7 +31,7 @@ const productData = [{
     "links":["https://as-tomas.github.io/Calculator-App/"]
   },  
   {
-    "id": 3,
+    "id": 4,
     "name": "Product DataBase",
     "description": "Back-End Java project for creating monthly data base and importing products from CSV file. ",
     "images": ["./data/backEnd.jpg",],
@@ -31,7 +39,7 @@ const productData = [{
     "links":["https://github.com/As-Tomas/ProductsImporterSQLServerAndManagemet"]
   },
   {
-    "id": 4,
+    "id": 5,
     "name": "FoofOut App",
     "description": "Android app, customers can register, make orders from selected restaurant and have it delivered straight to their homes.",
     "images": ["./data/Projects_Imgs/foodOut/delivery.jpg", "./data/Projects_Imgs/foodOut/login.jpg", "./data/Projects_Imgs/foodOut/menu.jpg"],
@@ -39,7 +47,7 @@ const productData = [{
     "links":["https://github.com/As-Tomas/foodOut_Android_App"]
   },
   {
-    "id": 5,
+    "id": 6,
     "name": "Product Scraper",
     "description": "Node.js automated product scraper by using Chrome/Chromium high-level API with the library Puppeteer and storing it in CSV file. ",
     "images": ["./data/backEnd.jpg",],
@@ -47,7 +55,7 @@ const productData = [{
     "links":["https://github.com/As-Tomas/Product_Scraper_VVS"]
   },
   {
-    "id": 6,
+    "id": 7,
     "name": "Dreams and diary App",
     "description": "Android app, to make notes in diary format.",
     "images": ["./data/Projects_Imgs/dreamDiary/evaluation.jpg", "./data/Projects_Imgs/dreamDiary/form.jpg", "./data/Projects_Imgs/dreamDiary/records.jpg"],
@@ -55,7 +63,7 @@ const productData = [{
     "links":["https://github.com/As-Tomas/Dreams_and_diary"]
   },
   {
-    "id": 7,
+    "id": 8,
     "name": "Speed Match game",
     "description": "Android Game: Different shapes slides from right to left, stops in the middle, and the player must choose buttons yes or no to answer. Does this symbol match the previous symbol? Score is calculated by the number of right answers in a given time.",
     "images": ["./data/Projects_Imgs/speedMatch/game.jpg", "./data/Projects_Imgs/speedMatch/rezults.jpg"],
@@ -63,7 +71,7 @@ const productData = [{
     "links":["https://github.com/As-Tomas/Speed_Match_game"]
   },
   {
-    "id": 8,
+    "id": 9,
     "name": "Eletricity cost calculator",
     "description": "Simple Android App for calculation of electricity prices and relief based on consumed kilowatts.",
     "images": ["./data/Projects_Imgs/eletricityCalc/history.jpg","./data/Projects_Imgs/eletricityCalc/mainActivity.jpg"],
@@ -71,7 +79,7 @@ const productData = [{
     "links":["https://github.com/As-Tomas/Eletricity_calculator"]
   },
   {
-    "id": 9,
+    "id": 10,
     "name": "PDF Invoice WP plugin ",
     "description": "On customer request edited WooCommerce-PDF-Invoice template with extended table",
     "images": ["./data/backEnd.jpg",],
